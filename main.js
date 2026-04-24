d155f39a1937054a121d3acc65f857146e612ad0
tomas fortte <tomasfortte@MacBook-Air-de-Tomas-2.local>
2026-04-24 04:38:34 -0300

    primer commit limpio sin video

---
+++ main.js
@@ -0,0 +1,36 @@
+/*
+console.log("oh mundo, HIII")
+
+console.log(2+2)
+
+console.log( "tomas" + "fortte")
+
+let nombre = "Tomas"
+
+console.log (nombre)
+
+let edad = 25
+
+edad = 26
+
+console.log(edad)
+
+const ciudad = "Buenos Aires"
+
+console.log(ciudad)
+
+function saludar (nombre) {
+console.log ("hola " + nombre )
+}
+
+saludar("Mendoza")
+saludar("aa")
+*/
+
+
+const boton = document.getElementById("btn-wpp")
+
+boton.addEventListener("click", function(){
+    console.log("clickearon whatsapp")
+})
+
